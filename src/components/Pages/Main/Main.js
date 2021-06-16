f1e58b166df9a41e20b5375823c7a990e3d2f57b
valeriiBorisenko <tjvell@yandex.ru>
2021-06-16 17:33:23 +0300

    feat: сделать хедер main

--- src/components/Pages/Main/Main.js
+++ src/components/Pages/Main/Main.js
@@ -1,6 +1,15 @@
+import Header from '../../Header/Header';
+import MainHeader from '../../ui/MainHeader/MainHeader';
+
 function Main() {
   return(
-    <section className="main" />
+    <section className="main">
+      <Header sectionClass="header_type_main">
+        <div className="header__logo" />
+        <MainHeader/>
+      </Header>
+      
+    </section>
   )
 };
 
