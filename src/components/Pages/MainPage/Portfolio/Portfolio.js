baca8a89680c238acb963f856558ef7ec9d43712
valeriiBorisenko <tjvell@yandex.ru>
2021-06-18 17:24:30 +0300

    fix: подправить портфолио ссылки и разрешения

--- src/components/Pages/MainPage/Portfolio/Portfolio.js
+++ src/components/Pages/MainPage/Portfolio/Portfolio.js
@@ -3,22 +3,19 @@ function Portfolio() {
     <section className="portfolio">
       <div className="portfolio__container">
         <h3 className="portfolio__title">Портфолио</h3>
-        <ul className="portfolio__links-container">
-          <li>
-            <a className="portfolio__link link-opacity" href="https://valeriiborisenko.github.io/Kaktus/" target="_blank" rel="noreferrer">
-              <p className="portfolio__text">Статичный сайт</p>
+        <ul className="portfolio__links">
+          <li className="portfolio__link-container link-opacity">
+            <a className="portfolio__link" href="https://valeriiborisenko.github.io/Kaktus/" target="_blank" rel="noreferrer">Статичный сайт
               <div className="portfolio__image" aria-label="стрелка повернутая" />
             </a>
           </li>
-          <li>
-            <a className="portfolio__link link-opacity" href="https://valeriiborisenko.github.io/russian-travel/index.html" target="_blank" rel="noreferrer">
-              <p className="portfolio__text" >Адаптивный сайт</p>
+          <li className="portfolio__link-container link-opacity">
+            <a className="portfolio__link" href="https://valeriiborisenko.github.io/russian-travel/index.html" target="_blank" rel="noreferrer">Адаптивный сайт
               <div className="portfolio__image" aria-label="стрелка повернутая" />
             </a>
           </li>
-          <li>
-            <a className="portfolio__link link-opacity" href="https://mesto-valeriib.nomoredomains.rocks/" target="_blank" rel="noreferrer">
-              <p className="portfolio__text">Одностраничное приложение</p>
+          <li className="portfolio__link-container link-opacity">
+            <a className="portfolio__link" href="https://mesto-valeriib.nomoredomains.rocks/" target="_blank" rel="noreferrer">Одностраничное приложение
               <div className="portfolio__image" aria-label="стрелка повернутая" />
             </a>
           </li>
