e85b1871a3f764c4c73eef740ccc45bbea45ced3
valeriiBorisenko <tjvell@yandex.ru>
2021-06-19 18:51:07 +0300

    feat: готовая страница 404

--- src/components/Pages/PageNotFound/PageNotFound.js
+++ src/components/Pages/PageNotFound/PageNotFound.js
@@ -1,6 +1,19 @@
+import PageLink from "../../ui/PageLink/PageLink";
+import { mainPageUrl } from "../../../utils/routes"
+
 function PageNotFound() {
   return (
-    <section className="page-not-found" />
+    <section className="page-not-found">
+      <div className="page-not-found__container">
+        <h2 className="page-not-found__title">404</h2>
+        <p className="page-not-found__description">Страница не найдена</p>
+      </div>
+      <PageLink
+        title="Назад"
+        href={`${mainPageUrl}`}
+        sectionClass="page-not-found__link"
+      />
+    </section>
   )
 };
 
